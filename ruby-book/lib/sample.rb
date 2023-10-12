e77a6a5a5669237ef6d261b0ad9ea9143f7f54c6
西野充洋 <tnhtx.17.m@gmail.com>
2023-10-12 21:35:47 +0900

    20231012

--- ruby-book/lib/sample.rb
+++ ruby-book/lib/sample.rb
@@ -804,3 +804,35 @@ fruits.each do |fruit|
   end
 end
 
+
+currencies = { 'japan' => 'yen', 'us' => 'dollar', 'india' =>  'rupee' }
+currencies.each do |key, value|
+  puts "#{key} : #{value}"
+end
+
+currencies.each do |key_value|
+  key = key_value[0]
+  value = key_value[1]
+  puts "#{key} : #{value}"
+end
+
+a = { 'x' => '1', 'y' => '2', 'z' => '3' }
+b = { 'x' => '1', 'y' => '2', 'z' => '3' }
+c = { 'x' => '1', 'z' => '3', 'y' => '2' }
+
+
+:apple.object_id
+:apple.object_id
+:apple.object_id
+:apple.object_id
+
+'apple'.object_id
+'apple'.object_id
+'apple'.object_id
+'apple'.object_id
+
+
+symbol = :apple
+
+currencies = { :japan => 'yen', :us => 'dollar', :india =>  'rupee' }
+currencies = { japan: 'yen', us: 'dollar', india: 'rupee' }
